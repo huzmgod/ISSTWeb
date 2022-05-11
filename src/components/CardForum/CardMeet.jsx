45fcf9c6e53b09a713127d3eb1006456827531a7
GonzaloNu <g.nunezmu@gmail.com>
2022-05-11 09:38:40 +0200

    modificados:     src/components/CardForum/CardMeet.jsx 	modificados:     src/pages/home/Home.jsx

--- src/components/CardForum/CardMeet.jsx
+++ src/components/CardForum/CardMeet.jsx
@@ -11,72 +11,101 @@ import { URLBACKEND } from '../../constants/constants';
 
 export default function CardMeet(props) {
 
-  const [authorName, setAuthorName] = React.useState("");
+    const [authorName, setAuthorName] = React.useState("");
 
-  const getAuthorName = async () => {
-    const res = await fetch(`${URLBACKEND}/usuario`);
-    const usuarios = await res.json();
+    const getAuthorName = async () => {
+        const res = await fetch(`${URLBACKEND}/usuario`);
+        const usuarios = await res.json();
 
-    for (const item of usuarios) {
-      if (item.id == props.autorId) {
-        setAuthorName(item.nombre);
-        return;
-      }
-    }
-  }
-  React.useEffect(() => {
-    getAuthorName();
-  }
-  );
-
-  const handleDelete = async () => {
-    props.setF5(!props.f5);
-    if (props.idLocal === props.autorId) {
-
-      alert("¿Seguro?. Esta acción no se puede deshacer");
-
-      const res = await fetch(`${URLBACKEND}/reunion/delete/${props.id}`);
-    } else {
-      alert("No puedes borrar una votación que no has creado");
-    }
-
-  }
-
-
-  return (
-
-
-    <Card sx={{ padding: '10px', minWidth: 275 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ height: '50px', width: '50px', bgcolor: "#FF7517" }} >
-            <img src={logo} alt="logo" style={{ width: '60px' }} />
-          </Avatar>
+        for (const item of usuarios) {
+            if (item.id == props.autorId) {
+                setAuthorName(item.nombre);
+                return;
+            }
         }
-        action={
-          <IconButton aria-label="settings">
-            <DeleteIcon onClick={handleDelete} />
-          </IconButton>
+    }
+    React.useEffect(() => {
+        getAuthorName();
+    }
+    );
+
+    const handleDelete = async () => {
+        props.setF5(!props.f5);
+        if (props.rolLocal == 0) {
+
+            alert("¿Seguro?. Esta acción no se puede deshacer");
+
+            const res = await fetch(`${URLBACKEND}/reunion/delete/${props.id}`);
+        } else {
+            alert("No puedes borrar una votación que no has creado");
         }
-        title={props.titulo}
-        subheader={authorName}
-      />
 
-      <CardContent>
+    }
 
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Esta votación caduca en 3 dias
-        </Typography>
 
-        <Typography color="text.secondary">
-          {/* {props.cuerpo} */}
-        </Typography>
-      </CardContent>
-      {/* <CardActions>
+    return (
+
+        (props.rolLocal == 0) ? (
+            <Card sx={{ padding: '10px', minWidth: 275 }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ height: '50px', width: '50px', bgcolor: "#FF7517" }} >
+                            <img src={logo} alt="logo" style={{ width: '60px' }} />
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <DeleteIcon onClick={handleDelete} />
+                        </IconButton>
+                    }
+                    title={props.titulo}
+                    subheader={props.fecha}
+                />
+
+                <CardContent>
+
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {props.presencial ? "La reunión será presencial" : "La reunión será online"}
+                    </Typography>
+
+                    <Typography color="text.secondary">
+                        {props.presencial ? "Localización" : "URL"}:{props.localizacion}
+                    </Typography>
+                </CardContent>
+                {/* <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions> */}
 
 
-    </Card>
-  );
+            </Card>)
+            : (
+                <Card sx={{ padding: '10px', minWidth: 275 }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ height: '50px', width: '50px', bgcolor: "#FF7517" }} >
+                                <img src={logo} alt="logo" style={{ width: '60px' }} />
+                            </Avatar>
+                        }
+                        title={props.motivo}
+                        subheader={props.fecha}
+                    />
+
+                    <CardContent>
+
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            {props.presencial ? "La reunión será presencial" : "La reunión será online"}
+                        </Typography>
+
+                        <Typography color="text.secondary">
+                            {props.presencial ? "Localización" : "URL"}:{props.localizacion}
+                        </Typography>
+                    </CardContent>
+                    {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+
+
+                </Card>
+            )
+    );
 }
